36643f193bfe7cd6575fe9c65eb11779928b9381
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-05-09 05:20:36 -0400

    Updates

---
+++ precache-manifest.f6f080fc0525e5c46437ffdf78043b4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf97e762c9de3f85d52f4a9fee9fea91",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "496fc277c184d6316521",
+    "url": "/to-do-list/static/css/main.0751910c.chunk.css"
+  },
+  {
+    "revision": "ed75045a4756dfa7de3a",
+    "url": "/to-do-list/static/js/2.60f2e126.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list/static/js/2.60f2e126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496fc277c184d6316521",
+    "url": "/to-do-list/static/js/main.b2122086.chunk.js"
+  },
+  {
+    "revision": "0fa796cae2e20245bd02",
+    "url": "/to-do-list/static/js/runtime-main.47aa806f.js"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/to-do-list/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  }
+]);
